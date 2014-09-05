ee60e5efefabeafcb0a4708de971f1d9430adffc
dan <dan@dan.com>
2014-09-05 16:27:57 +0100

    sorting

--- app/scripts/controllers/CtrlMain.js
+++ app/scripts/controllers/CtrlMain.js
@@ -1,22 +1,42 @@
 'use strict';
 
 angular.module('myApp.controller', [
-    'pp.graffiti'
-])
+	'pp.graffiti'
+	])
 .controller('MainCtrl', function ($scope, graffiti) {
+	$scope.ordered=false;
+	graffiti.search().then(function(response) {
+		$scope.groups = response.facets[0].children;
+	})
 
-    graffiti.search().then(function(response) {
-        $scope.groups = response.facets[0].children;
-    })
+	$scope.oneAtATime = true;
+	$scope.reorder=function(bool) {
+		$scope.ordered=bool;
+		console.log("$scope.ordered:",$scope.ordered);
+	};
+	$scope.sortingLog = [];
 
-    $scope.oneAtATime = true;
+	$scope.sortableOptions = {
+		stop: function(e, ui) {
+			for (var index in $scope.groups) {
+				$scope.groups[index].i = index;
+				console.log("g$scope.groups[index].name:",$scope.groups[index].name);
+				console.log("$scope.groups[index].i:",$scope.groups[index].i);
+			}
 
+			
+		}
+	};
+
+	
 })
 .controller('GroupController', function ($scope, graffiti) {
-        $scope.onClickRow = function(id) {
-            graffiti.search(id, null, 'posted-at-date').then(function(response) {
-                $scope.group.content = response.body;
-                console.log($scope.group.content)
-            })
-        };
+
+	$scope.onClickRow = function(id) {
+		graffiti.search(id, null, 'posted-at-date').then(function(response) {
+			$scope.group.content = response.body;
+			console.log($scope.group.content)
+		})
+	};
+
 })
